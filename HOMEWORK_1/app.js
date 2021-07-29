924c1c449f395939a1e25864c48e708677777fa3
Lena <lambitska92@gmail.com>
2021-07-29 21:46:52 +0300

    Added Task 5

--- HOMEWORK_1/app.js
+++ HOMEWORK_1/app.js
@@ -9,6 +9,21 @@ let color2 = 'black';
 console.log(color1, color2);
 color2 = color1;
 console.log(color1, color2);
+
+// Task 4
+let car = {
+    model: 'BMW',
+    year: 2019,
+    mileage: false,
+    owner: undefined,
+    convertibleTop: null
 }
 
+// Task 5
+let isAdult = confirm("Are you adalt?");
 
+if(isAdult === true){
+console.log("You are welcome !");
+} else {
+console.log("Bye 😈");
+}
