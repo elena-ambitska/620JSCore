a5b64aaff01871fd54b6204d4f9273d35ae566fb
Lena <lambitska92@gmail.com>
2021-07-29 21:22:22 +0300

    Task 3

--- HOMEWORK_1/app.js
+++ HOMEWORK_1/app.js
@@ -1,4 +1,14 @@
 // Homework1 
 // Task 2 
 let lastName = "Ambitska"
-console.log('My lastname: ' + lastName );
+console.log('My lastname: ' + lastName);
+
+// Task 3
+let color1 = 'white';
+let color2 = 'black';
+console.log(color1, color2);
+color2 = color1;
+console.log(color1, color2);
+}
+
+
